25fc90997d2e19f10caaae1c33acd982980bc603
dldnjswns31 <dldnjswns32@gmail.com>
2022-08-22 17:31:14 +0900

    feat: make counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { increase, decrease } from './countSlicer';
+import { useDispatch, useSelector } from 'react-redux';
+
+const ChangeValueBtn = styled.button`
+  display: inline-block;
+  width: 4rem;
+  height: 4rem;
+  margin: 1rem;
+  border-radius: 50%;
+  font-size: 2rem;
+  border: none;
+  background-color: #61dafb;
+  color: whitesmoke;
+  &:active {
+    transform: scale(0.9);
+  }
+`;
+
+const ButtonWrapper = styled.div``;
 
 function App() {
+  const count = useSelector((state) => state);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +31,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>count : {count}</p>
+        <ButtonWrapper>
+          <ChangeValueBtn onClick={() => dispatch(increase())}>
+            +
+          </ChangeValueBtn>
+          <ChangeValueBtn onClick={() => dispatch(decrease())}>
+            -
+          </ChangeValueBtn>
+        </ButtonWrapper>
       </header>
     </div>
   );
